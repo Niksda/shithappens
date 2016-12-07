6f7153e6cf9e89779e119798a4d1675ecfce4f87
Nikos <n1ksda@yahoo.de>
2016-12-07 14:25:33 +0100

    Html Includes
    funktioniert noch nicht local

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-w3IncludeHTML();
+$(document).ready(function() {
+    $("#mund").load("include/duenndarm.html");
+    $("#speiseroehre").load("include/duenndarm.html");
+    $("#magen").load("include/duenndarm.html");
+    $("#zwoelfdarm").load("include/duenndarm.html");
+    $("#duenndarm").load("include/duenndarm.html");
+    $("#dickdarm").load("include/duenndarm.html");
+    $("#mastdarm").load("include/duenndarm.html");
+});
