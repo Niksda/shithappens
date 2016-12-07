2756827748d6b08bab218fb15438b365e2e60aab
Nikos <n1ksda@yahoo.de>
2016-12-07 14:48:04 +0100

    Include geht nich
    ?!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
-    $("#mund").load("include/duenndarm.html");
-    $("#speiseroehre").load("include/duenndarm.html");
-    $("#magen").load("include/duenndarm.html");
-    $("#zwoelfdarm").load("include/duenndarm.html");
-    $("#duenndarm").load("include/duenndarm.html");
-    $("#dickdarm").load("include/duenndarm.html");
-    $("#mastdarm").load("include/duenndarm.html");
+    
 });
